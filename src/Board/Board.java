fc5ed871b4f96c5f8970c0f8a536588cfb55dbf0
waterfox-dev <waterfox80@gmail.com>
2023-02-13 12:09:25 +0100

    Add board and cell

---
+++ src/Board/Board.java
@@ -0,0 +1,14 @@
+package Board;
+
+import java.util.ArrayList;
+
+public abstract class Board
+{
+    public int height;
+    public int width;
+
+    public Board(int height, int width)
+    {
+
+    }
+}
